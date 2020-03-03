bcb79e09a4cb29814298b064918818a56212879f
Graziano <grazianos86@gmail.com>
2020-03-03 11:50:30 +0000

    fix cypress config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
   plugins: [
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-contentful',
