2f346eefec611be15d3cca7e3615c3b614db14fd
Graziano Statello <grazianos86@gmail.com>
2020-03-04 15:29:16 +0000

    add material-ui provider

---
+++ src/components/MainWrapper/MainWrapper.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Helmet} from 'react-helmet'
+
+import ThemeProvider from '@material-ui/styles/ThemeProvider'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {node} from 'prop-types'
+
+const MainWrapper = ({children}) => {
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <ThemeProvider>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </>
+  )
+}
+
+MainWrapper.propTypes = {
+  children: node.isRequired,
+}
+
+export default MainWrapper
