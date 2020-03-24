1a321c88b2d4fa37c939d16342e0d0da214fe60d
Graziano <grazianos86@gmail.com>
2020-03-24 15:45:14 +0000

    merge master

--- src/components/MainWrapper/MainWrapper.js
+++ src/components/MainWrapper/MainWrapper.js
@@ -6,15 +6,13 @@ import {node} from 'prop-types'
 import theme from '../../utils/theme'
 import Analytics from '../Analytics'
 
-const MainWrapper = ({element}) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Analytics />
-      {element}
-    </ThemeProvider>
-  )
-}
+const MainWrapper = ({element}) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Analytics />
+    {element}
+  </ThemeProvider>
+)
 
 MainWrapper.propTypes = {
   element: node.isRequired,
