1ae991ba43a60138b8c1db2db9385d90f4de5667
andgreendigital <agreen@and.digital>
2020-03-27 11:21:52 +0000

    :sparkles: Added link handler
    Co-authored-by: kkennedyAND <kkennedy@and.digital>

---
+++ src/components/LinkHandler/LinkHandler.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Link} from 'gatsby'
+
+const LinkHandler = ({url, newTab, children, ...props}) => {
+  if (url.startsWith('/')) {
+    return (
+      <Link to={url} {...props}>
+        {children}
+      </Link>
+    )
+  }
+  return newTab ? (
+    <a href={url} target="_blank" rel="noopener noreferrer" {...props}>
+      {children}
+    </a>
+  ) : (
+    <a href={url} {...props}>
+      {children}
+    </a>
+  )
+}
+
+LinkHandler.propTypes = {
+  url: PropTypes.string.isRequired,
+  newTab: PropTypes.bool,
+  children: PropTypes.node,
+}
+
+LinkHandler.defaultProps = {
+  children: null,
+  newTab: false,
+}
+
+export default LinkHandler
